5cd479ec1ed474cc395d56690f8b07a604ad8bd3
Skaushik274 <skaushik274@gmail.com>
2020-10-19 17:53:48 +0530

    first commit

---
+++ build/precache-manifest.065f0588bc79c2d15c38ae4cbb5134a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff8a0eff010a0e7787f2c538a0f13c11",
+    "url": "/index.html"
+  },
+  {
+    "revision": "13392afdc83bb4979696",
+    "url": "/static/css/main.64b2b73e.chunk.css"
+  },
+  {
+    "revision": "0e1bd2eccbe87bf3fc41",
+    "url": "/static/js/2.3e7f642d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.3e7f642d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13392afdc83bb4979696",
+    "url": "/static/js/main.b00c2adf.chunk.js"
+  },
+  {
+    "revision": "828933e7feaff28c01c6",
+    "url": "/static/js/runtime-main.8b9dda34.js"
+  }
+]);
---
+++ src/Container/Curriculum/Curriculum.js
@@ -0,0 +1,45 @@
+import Axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { logger } from '../../Utilities/CommonMethod'
+import { Link } from 'react-router-dom';
+import { ROUTE_ENDPOINT } from '../../Utilities/RouteEndPoint';
+import CurriculumPart from './../../Component/CurriculumPart/CurriculumPart';
+import styles from './Curriculum.module.css';
+import { classroom } from '../../Utilities/ApiDatas';
+const Curriculum = (props) => {
+    const [data, setdata] = useState({ accordian: [] })
+
+    useEffect(() => {
+        Axios.get('https://5ef9a09ebc5f8f0016c66d82.mockapi.io/ProjectDatas/3')
+            .then((resp) => {
+                const thatData = resp.data.value
+                    // const thatData = classroom
+                    .filter((item, pos) => pos === parseInt(props.match.params.id1))[0].modules
+                    .filter((item, pos) => pos === parseInt(props.match.params.id2))[0].curriculum
+                setdata({ ...thatData })
+                // document.write(JSON.stringify(classroom))
+            })
+            .catch(() => {
+                logger('data not found')
+            })
+    }, [props.match.params.id1, props.match.params.id2])
+
+    return (
+        <div>
+            <ul className={styles.Navtabs}>
+                <li className={styles.NavItem}>
+                    <Link to={`${ROUTE_ENDPOINT.CURRICULUM}/${props.match.params.id1}/${props.match.params.id2}`} className={`${styles.Navlink} ${styles.selected}`} >Units</Link>
+                </li>
+                <li className={styles.NavItem}>
+                    <Link to={`${ROUTE_ENDPOINT.GRADE}/${props.match.params.id1}/${props.match.params.id2}`} className={`${styles.Navlink}`} >Grade</Link>
+                </li>
+            </ul>
+            <div className={styles.MainContainer}>
+                <CurriculumPart id1={props.match.params.id1} id2={props.match.params.id2} data={data} />
+            </div>
+
+        </div>
+    )
+}
+
+export default Curriculum;
